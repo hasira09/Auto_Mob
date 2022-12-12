2d6d4ff3f74b36e7fb9b0f756b8c0a5689661d06
hasira09 <88621706+hasira09@users.noreply.github.com>
2022-12-12 18:27:41 +0530

    Menu Screen

--- main.py
+++ main.py
@@ -1,31 +1,15 @@
-from kivy.clock import Clock
-from kivy.core.window import Window
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.app import MDApp
-from kivymd.uix.textfield import MDTextField
+from kivy.core.window import Window
 
 Window.size = (310, 580)
 
 
-class LoginScreen(Screen):
-    pass
+class HomeScreen(MDApp):
 
-
-class SplashScreen(MDApp):
     def build(self):
-        global sm
-        sm = ScreenManager()
-        sm.add_widget(Builder.load_file("splash.kv"))
-        sm.add_widget(Builder.load_file("login.kv"))
-        return sm
-
-    def on_start(self):
-        Clock.schedule_once(self.login, 5)
-
-    def login(*args):
-        sm.current = "login"
+        self.theme_cls.primary_palette = "DeepPurple"
+        return Builder.load_file('home.kv')
 
 
-if __name__ == "__main__":
-    SplashScreen().run()
+HomeScreen().run()
